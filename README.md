6bee273c525ac18ea9548fecf5fc3afa044b0b87
Andrea Mamani <al.mamani@gmail.com>
2022-05-18 23:20:18 -0300

    Cuarta Version

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# Primera Version:
-Corresponde a la Primera Pre Entrega del proyecto de Javascript / Coderhouse
-
 # GitHub Pages:
+
 https://almamani.github.io/fiambreriaMamani/
+
+# Primera Version:
+
+Corresponde a la Primera Entrega del proyecto de Javascript / Coderhouse
+
+# Segunda Version:
+
+Corresponde al Desafío Complementario Clase 8 DOM de Javascript / Coderhouse
+
+# Tercera Version:
+
+Corresponde al Desafío Clase 9 Eventos de Javascript / Coderhouse
+
+# Cuarta Version:
+
+Corresponde a la Segunda Entrega del proyecto de Javascript / Coderhouse
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // VARIABLES y CONSTANTES GLOBALES
 const IVA = 1.21;
 let total = 0;
+const contenedorArt = document.getElementById("articulos");
+const tablaCompras = document.getElementById("tablaCompras");
+
+const articulos = [];
+const compras = [];
 
 // CLASES
 class Articulo {
@@ -14,155 +19,86 @@ class Articulo {
   }
 }
 
-class Venta {
-  constructor(desc, pre, des, can) {
-    this.descripcion = desc;
-    this.precio = pre;
-    this.descuento = des;
-    this.cantidad = can;
-    this.subtotal = 0;
-  }
-}
-
-// DECLARACION DE ARRAYS
-const articulos = [];
-const ventas = [];
-
-// FUNCIONES
-// Las tres siguientes funciones son en respuesta de los click en "cargar" de cada uno de los productos
-
-function respuestaClickSandwich() {
-  let cantidad = document.getElementsByClassName("cantidad")[0].value;
-  //Carga el array de ventas
-  ventas.push(
-    new Venta(
-      articulos[0].nombre,
-      articulos[0].precio,
-      articulos[0].descuento,
-      cantidad
-    )
-  );
-
-  res =
-    (articulos[0].precio - articulos[0].precio * articulos[0].descuento) *
-    IVA *
-    cantidad;
-  ventas[0].subtotal = res;
-  subtotal[0].innerHTML = "Subtotal: $" + res;
-  console.log(ventas[0].subtotal);
-}
-function respuestaClickPicada() {
-  let cantidad = document.getElementsByClassName("cantidad")[1].value;
-  //Carga el array de ventas
-  ventas.push(
-    new Venta(
-      articulos[1].nombre,
-      articulos[1].precio,
-      articulos[1].descuento,
-      cantidad
-    )
-  );
-
-  res =
-    (articulos[1].precio - articulos[1].precio * articulos[1].descuento) *
-    IVA *
-    cantidad;
-  ventas[1].subtotal = res;
-  subtotal[1].innerHTML = "Subtotal: $" + res;
-  console.log(ventas[1].subtotal);
-}
-
-function respuestaClickPizza() {
-  let cantidad = document.getElementsByClassName("cantidad")[2].value;
-  //Carga el array de ventas
-  ventas.push(
-    new Venta(
-      articulos[2].nombre,
-      articulos[2].precio,
-      articulos[2].descuento,
-      cantidad
-    )
-  );
-
-  res =
-    (articulos[2].precio - articulos[2].precio * articulos[2].descuento) *
-    IVA *
-    cantidad;
-  ventas[2].subtotal = res;
-  subtotal[2].innerHTML = "Subtotal: $" + res;
-  console.log(ventas[2].subtotal);
-}
-
-//MUESTRA LA VENTA REALIZADA
-function respuestaFinalizarVenta() {
-  // Acumula el total
-  total = ventas.reduce((acum, elem) => acum + elem.subtotal, 0);
-
-  //Mientras el total sea >0 (Exista una compra) muestra la venta
-  if (total > 0) {
-    let tituloVent = document.createElement("h3");
-    tituloVent.innerHTML =
-      "------------------ Su Compra: -----------------------";
-    document.body.append(tituloVent);
-
-    // Muestra el array con las venta efectuada
-    for (const venta of ventas) {
-      let contVenta = document.createElement("div");
-      contVenta.innerHTML = `<h4>${venta.descripcion}</h4>
-  <p>Cantidad: ${venta.cantidad}</p>
-  <p>Subtotal: $${venta.subtotal}</p>`;
-      document.body.appendChild(contVenta);
-    }
-
-    //Muestra el total de la venta
-    let totalFinal = document.createElement("h4");
-    totalFinal.innerHTML = `Total Compra: $${total}`;
-    document.body.append(totalFinal);
-  }
-}
-
-//SCRIPT PRINCIPAL
 //Carga de los artículos en el array artículos
 articulos.push(
-  new Articulo("1", "Sandwich por Docena", 900, "10%", 0.1, "img/img1.jpg")
+  new Articulo("0", "Sandwich Docena", 900, "10%", 0.1, "img/img1.jpg")
 );
 articulos.push(
-  new Articulo(
-    "2",
-    "Bandeja de Super Picada",
-    1000,
-    "15%",
-    0.15,
-    "img/img2.jpg"
-  )
+  new Articulo("1", "Bandeja de Picada", 1000, "15%", 0.15, "img/img2.jpg")
 );
 articulos.push(
-  new Articulo("3", "Pizza Lista Completa", 1100, "20%", 0.2, "img/img3.jpg")
+  new Articulo("2", "Pizza Completa", 1100, "20%", 0.2, "img/img3.jpg")
 );
 
-// Mostrar los artículos del array en el DOM
-for (const articulo of articulos) {
-  let contArticulo = document.createElement("div");
-  contArticulo.innerHTML = `<img src=${articulo.imagen} />' <h3>${articulo.nombre}</h3><b>$${articulo.precio} - </b><b>Dto: ${articulo.porcentaje} - </b><input class="cantidad" type="number" placeholder="cantidad" size="4"><input type="button" value="Agregar" class="agregar"><h4 class="subtotal"></h4><p>---------------------------------------------------------------------</p>`;
-  document.body.appendChild(contArticulo);
-}
+localStorage.setItem("articulosStorage", JSON.stringify(articulos));
 
-// Agrega un botón para que el usuario pueda confirmar la venta
-let btnFinalizarVenta = document.createElement("button");
-btnFinalizarVenta.innerHTML = `Finalizar Compra`;
-document.body.append(btnFinalizarVenta);
+// Genera cada artículo apartir del array de Articulos para luego agregarlos al DOM
+const crearArticulo = (item) => {
+  return `
+<div class="card" style="width: 18rem;">
+  <img src="${item.imagen}" class="card-img-top" alt="${item.nombre}">
+  <div class="card-body">
+    <h4 class="card-title">${item.nombre}</h4>
+    <p class="card-text">Precio: $${item.precio}</p>
+    <p class="card-text">Descuento: ${item.porcentaje}</p>
+    <b>Cantidad: <input type="number" class="cantidad w-25" min="0"/></b>
+     <a href="#" onclick="agregarCarrito(${item.codigo})"  class="btn btn-primary">Agregar</a><p><h5 class="subtotal"></h5></p>
+  </div>
+</div>
+`;
+};
 
-let subtotal = document.getElementsByClassName("subtotal");
+// Genera cada compra  a partir de las transacciones guardades en Local Storage para luego mostrarlas en el DOM
+const crearCompra = (item) => {
+  return `
+ <tr>
+      <td class=text-center>${item.cantidad}</td>
+      <td>${item.nombre}</td>
+      <td>$${item.subtotal}</td>
+      <td><img style="width: 40px" src="${item.imagen}" alt="imagen"></td>
+    </tr>
+`;
+};
 
-// Toma los click de los botones que se realicen en cada producto para agregar los mismos
-let btnArgregar = document.getElementsByClassName("agregar");
+//Agrega los artículos al DOM
+const agregarArticulos = (datos, nodo) => {
+  let acumulador = "";
+  datos.forEach((el) => {
+    acumulador += crearArticulo(el);
+  });
+  nodo.innerHTML = acumulador;
+};
 
-btnArgregar[0].addEventListener("click", respuestaClickSandwich);
+// Agrega las compras a una ventana modal para mostrar el resumen de las compras realizadas
+const agregarCompras = (datos, nodo) => {
+  let acumulador = "";
+  datos.forEach((elemento) => {
+    acumulador += crearCompra(elemento);
+  });
+  nodo.innerHTML = acumulador;
+};
 
-btnArgregar[1].addEventListener("click", respuestaClickPicada);
+//Cargar los artículos en el Dom
+const storageArticulos = JSON.parse(localStorage.getItem("articulosStorage"));
+agregarArticulos(storageArticulos, contenedorArt);
 
-btnArgregar[2].addEventListener("click", respuestaClickPizza);
+//Por cada click en un artículo calcula subtotal y guarda los datos de la compra en el Local Storage
+const agregarCarrito = (id) => {
+  let subtotal = document.getElementsByClassName("subtotal");
+  const seleccionado = articulos.find((item) => item.codigo == id);
+  let cantidad = document.getElementsByClassName("cantidad")[id].value;
+  resultado =
+    (seleccionado.precio - seleccionado.precio * seleccionado.descuento) *
+    IVA *
+    cantidad;
+  subtotal[id].innerHTML = "Subtotal: $" + resultado;
 
-// Toma el click del boton finalizar para mostrar los datos de la venta realizada
-btnFinalizarVenta = document.getElementsByTagName("button");
-btnFinalizarVenta[0].addEventListener("click", respuestaFinalizarVenta);
+  compras.push({
+    cantidad: cantidad,
+    nombre: seleccionado.nombre,
+    subtotal: resultado,
+    imagen: seleccionado.imagen,
+  });
+
+  agregarCompras(compras, tablaCompras);
+};
